b716a5e59cc8c5efa87f4d6702652900b76c6937
wpseemol <wpseemol@gmail.com>
2024-04-15 23:00:41 +0600

    bank account app is completed

--- bank-app.js
+++ bank-app.js
@@ -33,15 +33,17 @@ class BankAccount {
 const account1 = new BankAccount(1001, 'John Doe', 1000);
 const account2 = new BankAccount(1002, 'Jane Smith', 2000);
 
-console.log('Account 1:');
 account1.accountInfo();
 account1.deposit(500);
 account1.withdraw(200);
 account1.withdraw(1500);
 console.log('Updated Account 1 Balance:', account1.getBalance());
 
-console.log('Account 2:');
+console.log('------------------');
 account2.accountInfo();
 account2.deposit(1000);
 account2.withdraw(250);
 console.log('Updated Account 2 Balance:', account2.getBalance());
+
+// const accountNumber = prompt('Please enter bank account Number');
+// const ownerName = prompt('Please enter bank account Owner Name');
